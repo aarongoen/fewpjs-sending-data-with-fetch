9823bfc6c9440de1a573f59cd7811ed398990556
aarongoen <aarongoen2.0@gmail.com>
2021-02-20 11:31:04 -0500

    commit tree 88b7a0b7bc02d200e59d67cc08ea44bf107aa9c5

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+// let dogData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// };
+
+
+// fetch("http://localhost:3000/dogs", configObj)
+// .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object);
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   }); 
+  
+  function submitData(name, email) {
+
+let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({name, email})
+};
+
+return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.innerHTML = object.id;
+  })
+  .catch(function(error) {
+    alert("Un");
+    document.body.innerHTML = error.message;
+  }); 
+}
